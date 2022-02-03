050ec3e8a00c730d9df53cba471dd3a98bed0315
erastian <erastian@gmail.com>
2022-02-03 03:14:21 +0300

    Finish adaptive for header & menu. Start slider. Add arrow icons.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,12 @@
 import * as customFunctions from './modules/functions.js';
 
 customFunctions.isWebp();
+
+
+let menuBtn = document.querySelector('.nav__icon');
+let nav = document.querySelector('.nav');
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('_active');
+    nav.classList.toggle('_active');
+})
